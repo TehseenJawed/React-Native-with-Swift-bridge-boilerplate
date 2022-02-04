c27e36b60c7cdb1ae6695d4a410a0f4080a8f08f
imrandipixels <imrandipixels@gmail.com>
2022-02-04 06:35:06 +0500

    Integrated Swift bridge in React Native for Android, and iOS

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Platform, View, Text, NativeModules, TouchableOpacity } from "react-native";
+
+const HelloWorld = NativeModules.HelloWorldModule;
+
+export default class App extends Component{
+  render(){
+    console.log(NativeModules.HelloWorld);
+    return(
+      <View style={{flex:1, alignItems: "center", justifyContent:'center'}}>
+        <TouchableOpacity onPress={()=>{
+          if(Platform.OS == "android"){
+            NativeModules.HelloWorldModule.ShowMessage("This is first time we are creating bridge. :)", 5000);
+          }
+          else if(Platform.OS == "ios"){
+            NativeModules.HelloWorld.ShowMessage("!its working!", 0.5);
+          }
+        }}>
+          <Text>Click Me!</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
